01aa2c096bc6e92a9c143c3677dcfa91eaf74c4c
Andreyome <118749442+Andreyome@users.noreply.github.com>
2023-03-31 22:03:36 +0300

    Update ThreadSafeSingleton.java

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -5,6 +5,9 @@ public static volatile ThreadSafeSingleton instance = new ThreadSafeSingleton();
 private ThreadSafeSingleton (){
 }
 public static synchronized ThreadSafeSingleton getInstance(){
+if (instance == null){
+    instance = new ThreadSafeSingleton();
+}
     return instance;
 }
 }
