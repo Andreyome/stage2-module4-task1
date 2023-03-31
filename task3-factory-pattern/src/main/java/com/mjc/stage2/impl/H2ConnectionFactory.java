686ae7d430dc05e1d0ae766d420f3d19b4231760
Andreyome <118749442+Andreyome@users.noreply.github.com>
2023-03-31 21:56:19 +0300

    commit 1

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,26 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+     Connection con = null;
+        Properties prop = new Properties();
+        try {
+        prop.load(getClass().getResourceAsStream("/h2database.properties"));
+        return DriverManager.getConnection("jdbc:h2:~/test",prop);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     // Write your code here!
 }
 
